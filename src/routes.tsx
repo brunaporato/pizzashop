b303ae7132d906ad9fc4e51e4083a50c4f040719
bporato <brunaporato@pandanjo.com>
2024-07-16 07:57:58 -0300

    feat: layout for routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/signin'
 
 export const router = createBrowserRouter([
-  { path: '/', element: <Dashboard /> },
-  { path: '/signin', element: <SignIn /> },
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [{ path: '/', element: <Dashboard /> }],
+  },
+
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [{ path: '/signin', element: <SignIn /> }],
+  },
 ])
