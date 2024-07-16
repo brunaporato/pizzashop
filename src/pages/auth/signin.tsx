d0cee0865aa61b643ec043111c0ad2fbe483cdf8
bporato <brunaporato@pandanjo.com>
2024-07-16 08:28:31 -0300

    styles: auth layout + signin page

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,10 +1,36 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
 export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <h1>SignIn</h1>
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Access Dashboard
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Track your sales through partner dashboard!
+            </p>
+          </div>
+
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Your e-mail</Label>
+              <Input id="email" type="email" />
+            </div>
+
+            <Button type="submit" className="w-full">
+              Access dashboard
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
