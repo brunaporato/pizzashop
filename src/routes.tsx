36264f64cc37afe44606fd0ede64fdfe0628bd3a
brunaporato <brunaporato@gmail.com>
2024-07-18 16:21:08 -0300

    refactor: organize routes imports

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,11 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
+import { NotFound } from './pages/404'
 import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/signin'
 import { SignUp } from './pages/auth/signup'
-import { NotFound } from "./pages/404"
 
 export const router = createBrowserRouter([
   {
