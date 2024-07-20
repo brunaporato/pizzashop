8f99ae6c6b2b1c4cf8d19a63909eb4748d430e89
brunaporato <brunaporato@gmail.com>
2024-07-20 09:56:05 -0300

    feat: fetching user and restaurant data for header + styling skeleton for loading state

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,4 +4,13 @@ import { env } from '@/env'
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
+  withCredentials: true,
 })
+
+if (env.VITE_API_ENABLE_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    return config
+  })
+}
